5d4524c2da6eba8eeecdfdcfcd88252529608d8b
Jyun Rong Liu <jyunrl@uci.edu>
2024-07-23 00:39:42 -0700

    Implemented basic header and navbar

---
+++ src/app/navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <div className="flex justify-between items-center p-4">
+      <header className="text-2xl font-bold ml-[75px]">James Liu.</header>
+      <nav className="mr-[50px] hidden flex-col gap-6 text-xl font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-lg lg:gap-6 lg:text-xl">
+        <Link
+          href="#"
+          className="text-muted-foreground transition-colors hover:text-foreground text-lg"
+        >
+          About
+        </Link>
+        <Link
+          href="#"
+          className="text-muted-foreground transition-colors hover:text-foreground text-lg"
+        >
+          Skill
+        </Link>
+        <Link
+          href="#"
+          className="text-muted-foreground transition-colors hover:text-foreground text-lg"
+        >
+          Projects
+        </Link>
+        <Link
+          href="#"
+          className="text-muted-foreground transition-colors hover:text-foreground text-lg"
+        >
+          Contact Me
+        </Link>
+      </nav>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Badge } from "@/components/ui/badge";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -18,64 +6,32 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Textarea } from "@/components/ui/textarea";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import NavBar from "./navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 text-xl">
-          James Liu.
-        </header>
+    <div>
+      <NavBar></NavBar>
+      <main>
+        <div>
+          <div className="">
+            <img
+              src="profile-pic.jpg"
+              alt="profile-pic"
+              className="rounded-lg"
+            />
+          </div>
 
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
-            <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
-              <img
-                src="profile-pic.jpg"
-                alt="profile-pic"
-                className="rounded-lg"
-              />
+          <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
+            <Card>
+              <CardHeader>
+                <CardTitle>Bio</CardTitle>
+                <CardDescription>Hi I am James Liu</CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+            </Card>
 
-              <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Bio</CardTitle>
-                    <CardDescription>
-                      Lipsum dolor sit amet, consectetur adipiscing elit
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent></CardContent>
-                </Card>
-
-                <Card>
+            {/* <Card>
                   <CardHeader>
                     <CardTitle>Skills</CardTitle>
                     <CardDescription>
@@ -91,11 +47,23 @@ export default function Home() {
                   </CardHeader>
                   <CardContent></CardContent>
                 </Card>
-              </div>
-            </div>
+
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Project</CardTitle>
+                  </CardHeader>
+                  <CardContent></CardContent>
+                </Card>
+
+                <Card>
+                  <CardHeader>
+                    <CardTitle>About Me</CardTitle>
+                  </CardHeader>
+                  <CardContent></CardContent>
+                </Card> */}
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   );
 }
